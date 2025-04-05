5ef2081de53b21c38357bc6c887d33e5d4afab38
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-04-05 17:50:31 +0530

    update lexer

--- src/Part2/lexer.tab.c
+++ src/Part2/lexer.tab.c
@@ -1425,7 +1425,7 @@ yyreduce:
           }
       }
       char err_token[256];
-      snprintf(err_token, sizeof(err_token), "%-20s  %s", invalid_type, "ERROR (invalid data type)");
+      snprintf(err_token, sizeof(err_token), "%-20s  %s", invalid_type, "Error: invalid data type)");
       token_output[token_output_count++] = strdup(err_token);
       char err_msg[256];
       snprintf(err_msg, sizeof(err_msg), "%s (invalid data type)", invalid_type);
--- src/Part2/lexer.y
+++ src/Part2/lexer.y
@@ -93,7 +93,7 @@ type:
           }
       }
       char err_token[256];
-      snprintf(err_token, sizeof(err_token), "%-20s  %s", invalid_type, "Error: invalid data type)");
+      snprintf(err_token, sizeof(err_token), "%-20s  %s", invalid_type, "Error: invalid data type");
       token_output[token_output_count++] = strdup(err_token);
       char err_msg[256];
       snprintf(err_msg, sizeof(err_msg), "%s (invalid data type)", invalid_type);
